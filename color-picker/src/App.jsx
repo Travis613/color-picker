1f08155e2b6ed49d10fd53d0fe9c92378f1ca20b
Travis <travism.github@gmail.com>
2026-03-23 10:35:11 -0400

    added buttons

--- color-picker/src/App.jsx
+++ color-picker/src/App.jsx
@@ -1,5 +1,17 @@
+import { ColorButton } from "./components/ColorButton";
+
 function App() {
-  return <></>;
+  const colors = ["blue", "green", "red", "random"];
+
+  const listItems = colors.map((color) => (
+    <ColorButton color={color}></ColorButton>
+  ));
+
+  return (
+    <>
+      <div>{listItems}</div>
+    </>
+  );
 }
 
 export default App;
---
+++ color-picker/src/components/ColorButton.jsx
@@ -0,0 +1,3 @@
+export function ColorButton({ color }) {
+  return <button>{color}</button>;
+}
