7fcefb779fb0289c135f488c776050bf556d0605
Travis <travism.github@gmail.com>
2026-03-25 11:32:05 -0400

    added state to the color box

--- color-picker/src/components/ColorContainer.jsx
+++ color-picker/src/components/ColorContainer.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export function ColorContainer() {
+  let [color, setColor] = useState("white");
+
   return (
     <div
-      style={{ height: "500px", width: "500px", borderStyle: "solid" }}
+      style={{
+        height: "500px",
+        width: "500px",
+        borderStyle: "solid",
+        backgroundColor: { color },
+      }}
     ></div>
   );
 }
